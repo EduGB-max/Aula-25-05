593053a37a27314f8e100572a8e26f96274a9c2a
EduGB-max <84384908+EduGB-max@users.noreply.github.com>
2022-05-25 12:36:32 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const conn = async()=>{
     //mongoAtlas
-    const atlas = await mongoose.connect('mongodb+srv://useradmin:1234@fiaptecnico.zg8lq.mongodb.net/todo_list')
+    const atlas = await mongoose.connect('seu link')
 }
 
 module.exports = conn
